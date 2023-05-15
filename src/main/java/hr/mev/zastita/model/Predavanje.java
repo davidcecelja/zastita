769b357107bf227cd25c4d88a5c9db1402792559
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2023-05-15 14:02:56 +0200

    -add models

---
+++ src/main/java/hr/mev/zastita/model/Predavanje.java
@@ -0,0 +1,72 @@
+package hr.mev.zastita.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="predavanje")
+public class Predavanje {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_predavanje;
+	
+	@Column
+	private String naziv_predavanja;
+	
+	@Column
+	private LocalDate datum_predavanja;
+	
+	@Column
+	private int broj_studenata;
+	
+	public Predavanje() {
+		super();
+	}
+
+	public Predavanje(long id_predavanje, String naziv_predavanja, LocalDate datum_predavanja, int broj_studenata) {
+		super();
+		this.id_predavanje = id_predavanje;
+		this.naziv_predavanja = naziv_predavanja;
+		this.datum_predavanja = datum_predavanja;
+		this.broj_studenata = broj_studenata;
+	}
+
+	public long getId_predavanje() {
+		return id_predavanje;
+	}
+
+	public void setId_predavanje(long id_predavanje) {
+		this.id_predavanje = id_predavanje;
+	}
+
+	public String getNaziv_predavanja() {
+		return naziv_predavanja;
+	}
+
+	public void setNaziv_predavanja(String naziv_predavanja) {
+		this.naziv_predavanja = naziv_predavanja;
+	}
+
+	public LocalDate getDatum_predavanja() {
+		return datum_predavanja;
+	}
+
+	public void setDatum_predavanja(LocalDate datum_predavanja) {
+		this.datum_predavanja = datum_predavanja;
+	}
+
+	public int getBroj_studenata() {
+		return broj_studenata;
+	}
+
+	public void setBroj_studenata(int broj_studenata) {
+		this.broj_studenata = broj_studenata;
+	}
+}
---
+++ src/main/java/hr/mev/zastita/model/Student.java
@@ -0,0 +1,82 @@
+package hr.mev.zastita.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_student;
+	
+	@Column
+	private String ime_student;
+	
+	@Column
+	private String prezime_student;
+	
+	@Column
+	private String email_student;
+	
+	@Column
+	private String lozinka_student;
+	
+	public Student() {
+		super();
+	}
+	
+	public Student(long id_student, String ime_student, String prezime_student, String email_student,
+			String lozinka_student) {
+		super();
+		this.id_student = id_student;
+		this.ime_student = ime_student;
+		this.prezime_student = prezime_student;
+		this.email_student = email_student;
+		this.lozinka_student = lozinka_student;
+	}
+	
+	public long getId_student() {
+		return id_student;
+	}
+	
+	public void setId_student(long id_student) {
+		this.id_student = id_student;
+	}
+	
+	public String getIme_student() {
+		return ime_student;
+	}
+	
+	public void setIme_student(String ime_student) {
+		this.ime_student = ime_student;
+	}
+	
+	public String getPrezime_student() {
+		return prezime_student;
+	}
+	
+	public void setPrezime_student(String prezime_student) {
+		this.prezime_student = prezime_student;
+	}
+	
+	public String getEmail_student() {
+		return email_student;
+	}
+	
+	public void setEmail_student(String email_student) {
+		this.email_student = email_student;
+	}
+	
+	public String getLozinka_student() {
+		return lozinka_student;
+	}
+	public void setLozinka_student(String lozinka_student) {
+		this.lozinka_student = lozinka_student;
+	}
+}
