99be4262bb563ef3eba60334ccb973a40fc25ad9
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2023-05-15 11:07:41 +0200

    prvi commit

---
+++ src/main/java/hr/mev/zastita/ZastitaApplication.java
@@ -0,0 +1,13 @@
+package hr.mev.zastita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZastitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZastitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/mev/zastita/ZastitaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.mev.zastita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZastitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
