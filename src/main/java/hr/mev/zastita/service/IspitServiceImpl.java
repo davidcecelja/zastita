a98cbe3cf2e620973dae6a3d42439318e5718265
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2023-05-24 23:26:44 +0200

    -add ispitserviceimpl

--- src/main/java/hr/mev/zastita/service/IspitServiceImpl.java
+++ src/main/java/hr/mev/zastita/service/IspitServiceImpl.java
@@ -1,31 +1,56 @@
 package hr.mev.zastita.service;
 
+import java.util.Optional;
+
+import hr.mev.zastita.exceptions.ResourceNotFoundException;
 import hr.mev.zastita.model.Ispit;
+import hr.mev.zastita.repository.IspitRepository;
 
 public class IspitServiceImpl implements IspitService{
 
+	private IspitRepository ispitRepository;
+	
 	@Override
 	public Ispit createIspit(Ispit ispit) {
-		// TODO Auto-generated method stub
-		return null;
+		return ispitRepository.save(ispit);
 	}
 
 	@Override
-	public Ispit updateIspit(Ispit ispit) {
-		// TODO Auto-generated method stub
-		return null;
+	public Ispit updateIspit(Ispit ispit) throws ResourceNotFoundException {
+	
+		Optional<Ispit> productDB = this.ispitRepository.findById(ispit.getId_ispit());
+		if(productDB.isPresent()) {
+			Ispit ispitUpdate = productDB.get();
+			ispitUpdate.setNaziv_ispit(ispit.getNaziv_ispit());
+			ispitUpdate.setDatum_ispita(ispit.getDatum_ispita());
+			ispitRepository.save(ispitUpdate);
+			return ispitUpdate;
+		} else {
+			throw new ResourceNotFoundException("Zapis nije pronađen : " + ispit.getId_ispit());
+		}		
 	}
 
 	@Override
 	public Ispit getIspit(long id_ispit) {
-		// TODO Auto-generated method stub
-		return null;
+		if(id_ispit == 0)
+			return new Ispit();
+		
+		Optional<Ispit> productDB = this.ispitRepository.findById(id_ispit);
+		if(productDB.isPresent()) {
+			return productDB.get();
+		} else {
+			return new Ispit();
+		}
 	}
 
 	@Override
 	public void deleteIspit(long id_ispit) {
-		// TODO Auto-generated method stub
-		
+		Optional<Ispit> productDB = this.ispitRepository.findById(id_ispit);
+		if(productDB.isPresent()) {
+			this.ispitRepository.delete(productDB.get());
+		} else {
+			throw new ResourceNotFoundException("Zapis nije pronađen!");
+		}
 	}
 
 }
