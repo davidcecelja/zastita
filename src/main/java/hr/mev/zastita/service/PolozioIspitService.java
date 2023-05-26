9a04399bea5de66ba2329e65945001d6d7771407
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2023-05-26 23:42:46 +0200

    -add baza

--- src/main/java/hr/mev/zastita/service/PolozioIspitService.java
+++ src/main/java/hr/mev/zastita/service/PolozioIspitService.java
@@ -1,5 +1,14 @@
 package hr.mev.zastita.service;
 
-public interface PolozioIspitService {
+import hr.mev.zastita.model.PolozioIspit;
 
+public interface PolozioIspitService {
+	
+	PolozioIspit createPolozio(PolozioIspit polozen);
+
+	PolozioIspit updatePolozio(PolozioIspit polozen);
+	
+	PolozioIspit getPolozio(long id_polozen);
+
+	void deletePolozio(long id_polozen);
 }
--- src/main/java/hr/mev/zastita/service/PolozioIspitServiceImpl.java
+++ src/main/java/hr/mev/zastita/service/PolozioIspitServiceImpl.java
@@ -1,5 +1,54 @@
 package hr.mev.zastita.service;
 
-public class PolozioIspitServiceImpl {
+import java.util.Optional;
 
+import hr.mev.zastita.exceptions.ResourceNotFoundException;
+import hr.mev.zastita.model.PolozioIspit;
+import hr.mev.zastita.repository.PolozioIspitRepository;
+
+public class PolozioIspitServiceImpl implements PolozioIspitService{
+
+	private PolozioIspitRepository polozioRepository;
+	
+	@Override
+	public PolozioIspit createPolozio(PolozioIspit polozen) {
+		return polozioRepository.save(polozen);
+	}
+
+	@Override
+	public PolozioIspit updatePolozio(PolozioIspit polozen) {
+		Optional<PolozioIspit> productDB = this.polozioRepository.findById(polozen.getId_polozen());
+		if(productDB.isPresent()) {
+			PolozioIspit polozioUpdate = productDB.get();
+			polozioUpdate.setDatumPolaganja(polozen.getDatumPolaganja());
+			polozioRepository.save(polozioUpdate);
+			return polozioUpdate;
+		} else {
+			throw new ResourceNotFoundException("Zapis nije pronađen : " + polozen.getId_polozen());
+		}		
+	}
+
+	@Override
+	public PolozioIspit getPolozio(long id_polozen) {
+
+		if(id_polozen == 0) 
+			return new PolozioIspit();
+		
+		Optional<PolozioIspit> productDB = this.polozioRepository.findById(id_polozen);
+		if(productDB.isPresent()) {
+			return productDB.get();
+		} else {
+			return new PolozioIspit();
+		}		
+	}
+
+	@Override
+	public void deletePolozio(long id_polozen) {
+		Optional<PolozioIspit> productDB = this.polozioRepository.findById(id_polozen);
+		if(productDB.isPresent()) {
+			this.polozioRepository.delete(productDB.get());
+		} else {
+			throw new ResourceNotFoundException("Zapis nije pronađen.");
+		}
+	}
 }
