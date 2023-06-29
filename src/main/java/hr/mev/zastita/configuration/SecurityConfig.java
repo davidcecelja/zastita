07b23613c349a5835a23617483891d09fd0c1743
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2023-06-29 09:48:25 +0200

    -add fixes

--- src/main/java/hr/mev/zastita/configuration/SecurityConfig.java
+++ src/main/java/hr/mev/zastita/configuration/SecurityConfig.java
@@ -1,19 +1,66 @@
 package hr.mev.zastita.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.sql.DataSource;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
-    
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("korisnik")
-                .password("{noop}lozinka") 
-                .roles("nastavnik", "student");
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler() {
+        return new StranicaHandler();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity)
+            throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/h2-console/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .successHandler(myAuthenticationSuccessHandler());
+
+        httpSecurity.csrf().ignoringRequestMatchers("/h2-console/**");
+        httpSecurity.headers()
+                .frameOptions()
+                .sameOrigin();
+        return httpSecurity.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT email, lozinka, enabled "
+                        + "from korisnik "
+                        + "where email = ?")
+                .authoritiesByUsernameQuery("select email, uloga "
+                        + "from korisnik "
+                        + "where email = ?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
+
--- src/main/java/hr/mev/zastita/controller/KorisnikController.java
+++ src/main/java/hr/mev/zastita/controller/KorisnikController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import java.util.ArrayList;
 
 import hr.mev.zastita.model.Korisnik;
 import hr.mev.zastita.service.KorisnikService;
@@ -21,21 +22,17 @@ public class KorisnikController {
 	@Autowired
 	private KorisnikService service;
 	
-	@GetMapping("/")
-	public String pocetnaStranica(Model model, Korisnik korisnik) {
-		String uloga = korisnik.getUloga();
-	       if(uloga.equals("student")) {
-	    	   return "pocetna_student";
-	       } else if(uloga.equals("nastavnik")) {
-	    	   return "pocetna_nastavnik";
-	       }
-		return uloga;
+	@GetMapping("/pocetna-student")
+	public String pocetnaStranica(Model model) {
+		model.addAttribute("korisnik", new Korisnik());
+		model.addAttribute("korisnici", new ArrayList<>());
+		return "pocetna_student";
 	}
 	
 	@GetMapping("/novi")
 	public String noviKorisnikGet(Model model) {
 		Korisnik korisnik = new Korisnik();
-		model.addAttribute("korisnik", korisnik);
+		model.addAttribute("korisnik", new ArrayList<>());
 		return "pocetna_nastavnik";
 	}
 	
@@ -44,6 +41,42 @@ public class KorisnikController {
 		service.registracijaKorisnika(korisnik);
 		return "redirect:/login/"; 
 	}
+	
+	
+	////////////////////////////////////////
+	@GetMapping("/pocetna-student")
+    public String pocetnaStranicaStudent(Model model) {
+        model.addAttribute("korisnik", new Korisnik());
+        model.addAttribute("korisnici", new ArrayList<>());
+        return "pocetna_student";
+    }
+
+    @GetMapping("/pocetna-nastavnik")
+    public String pocetnaStranicaNastavnik(Model model) {
+        model.addAttribute("korisnik", new Korisnik());
+        model.addAttribute("korisnici", new ArrayList<>());
+        return "pocetna_nastavnik";
+    }
+
+    @GetMapping("/novi")
+    public String noviKorisnikGet(Model model) {
+        Korisnik korisnik = new Korisnik();
+        model.addAttribute("korisnik", korisnik);
+        return "pocetna_nastavnik";
+    }
+
+    @PostMapping("/novi")
+    public String dodajKorisnikPost(@ModelAttribute("korisnik") Korisnik korisnik) {
+        service.registracijaKorisnika(korisnik);
+        return "redirect:/login/";
+    }
+	////////////////////////////////////////////////////
+	
+	
+	
+	
+	
+	
 	
 	@GetMapping("/uredi_korisnik/{id}")
 	public ModelAndView urediKorisnikGet(@PathVariable("id") long id) {
@@ -108,3 +141,69 @@ public class KorisnikController {
 	}
 }
 
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @GetMapping("/uredi_korisnik/{id}")
+    public ModelAndView urediKorisnikGet(@PathVariable("id") long id) {
+        ModelAndView mav = new ModelAndView("uredi_korisnika");
+        Korisnik korisnik = service.getKorisnik(id);
+        mav.addObject("korisnik", korisnik);
+        return mav;
+    }
+
+    @PostMapping("/uredi_korisnik")
+    public String spremiKorisnika(@ModelAttribute("korisnik") Korisnik korisnik) {
+        service.updateKorisnik(korisnik);
+        return "redirect:/login/";
+    }
+
+    @GetMapping("/korisnik/brisi/{id}")
+    public String brisiKorisnik(@PathVariable(name = "id") long id) {
+        service.deleteKorisnik(id);
+        return "redirect:/login/";
+    }
+
+    @GetMapping("/registracija")
+    public String prikaziFormuRegistracije(Model model) {
+        model.addAttribute("korisnik", new Korisnik());
+        return "korisnik";
+    }
+
+    @PostMapping("/registracija")
+    public String registracijaKorisnika(Korisnik korisnik) {
+        service.registracijaKorisnika(korisnik);
+        return "redirect:/registracija";
+    }
+
+    @GetMapping("/login")
+    public String prikaziFormuPrijave() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String prijavaKorisnika(@RequestBody Korisnik korisnik, RedirectAttributes redirectAttributes) {
+        try {
+            String email = korisnik.getEmail();
+            String lozinka = korisnik.getLozinka();
+            String uloga = service.prijavaKorisnika(email, lozinka);
+            if (uloga.equals("student")) {
+                return "redirect:/pocetna_student";
+            } else if (uloga.equals("nastavnik")) {
+                return "redirect:/pocetna_nastavnik";
+            } else {
+                throw new IllegalStateException("Nepoznata uloga korisnika");
+            }
+        } catch (BadCredentialsException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Pogrešna e-mail adresa ili lozinka");
+            return "redirect:/login";
+        }
+    }
+
+    @GetMapping("/odjava")
+    public String odjavaKorisnika() {
+        service.odjavaKorisnika();
+        return "redirect:/login";
+    }
+}
